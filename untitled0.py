cf28bdfd90c089493ab92fbbf10fc07797982cb9
Antoine Grosjean <ant.grosjean@gmail.com>
2021-02-11 21:56:48 +0100

    New_version

--- untitled0.py
+++ untitled0.py
@@ -1,6 +1,7 @@
-a = 2 
-print(a)
-aobdsjx
-Antoine Grosjean
-Arnaud Duvivier
+"""
+Created on Thu Feb 11 21:54:55 2021
 
+@author: Antoine Grojean Arnaud Duvivier Olivier Van de Velde
+"""
+
+#Voilà le fichier est nettoyé
