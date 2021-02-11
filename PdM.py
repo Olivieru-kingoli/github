6e51c167179ee18c74b73df27e10da300586d980
AntoineGRJN <78928076+AntoineGRJN@users.noreply.github.com>
2021-02-11 21:53:50 +0100

    Delete PdM.py

--- PdM.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 11 21:40:46 2021
-
-@author: Antoine Grosjean Arnaud Duvivier Olivier Van de Velde
-"""
-
-#Voici un nouveau fichier un peu plus propre
